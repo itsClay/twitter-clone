27396ef31a1b18565a67bd4d10e4dabb408a4258
Clay Shaw <Claydshaw@gmail.com>
2017-10-08 20:43:21 -0700

    hook up graphql

---
+++ server/src/graphql/schema.js
@@ -0,0 +1,15 @@
+// main file for react schema
+export default `
+  type Tweet {
+    _id: String
+    text: String
+  }
+
+  type Query {
+    getTweets: [Tweet]
+  }
+
+  schema {
+    query: Query
+  }
+`;
--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,42 @@
 /* eslint-disable no-console */
 
 import express from 'express';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 import bodyParser from 'body-parser';
+import { createServer } from 'http';
+
 import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers/index';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
 
-// applies middleware
 app.use(bodyParser.json());
 
-app.listen(PORT, err => {
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: constants.GRAPHQL_PATH
+}));
+
+app.use(constants.GRAPHQL_PATH, graphqlExpress({
+  schema
+}));
+
+const graphQLServer = createServer(app);
+
+// applies middleware
+
+
+graphQLServer.listen(constants.PORT, err => {
   if(err) {
     console.error(err);
   } else {
-    console.log(`app listen on port: ${PORT}`);
+    console.log(`app listen on port: ${constants.PORT}`);
   }
 });
